a5a4adc1e19961fd4f046b3d09db2d0bd2690e65
Arfiikins <afallajan@gmail.com>
2024-06-07 20:04:51 +0800

    added comments on aws_static_website

--- aws_s3_static_website/main.tf
+++ aws_s3_static_website/main.tf
@@ -1,8 +1,9 @@
-# This is an IaC to deploy an html file in S3 and can be access from public
+# This is an IaC to upload an html file in S3 and can be accessed by from anywhere
 provider "aws" {
   region     = "us-west-1"
 }
 
+# Creating a JSON document IAM policy 
 data "aws_iam_policy_document" "allow_s3GetObject_to_all" {
   statement {
     principals {
@@ -20,18 +21,21 @@ data "aws_iam_policy_document" "allow_s3GetObject_to_all" {
   }
 }
 
+# Creating an S3 bucket
 resource "aws_s3_bucket" "static_website" {
   bucket = "flappy-bird-static-website"
   
 }
 
-resource "aws_s3_bucket_versioning" "static-websit-versioning-enabled" { # Enabled versioning
+# Enable Versioning (optional)
+resource "aws_s3_bucket_versioning" "static-websit-versioning-enabled" { 
   bucket = aws_s3_bucket.static_website.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+# Enable static website 
 resource "aws_s3_bucket_website_configuration" "static-website-config" {
   bucket = aws_s3_bucket.static_website.id
 
@@ -45,11 +49,13 @@ resource "aws_s3_bucket_website_configuration" "static-website-config" {
 
 }
 
+# Create an S3 Bucket Policy from the data we have created aboce and attach it to the created bucket
 resource "aws_s3_bucket_policy" "static_website_policy" {
   bucket = aws_s3_bucket.static_website.id
 
   policy = data.aws_iam_policy_document.allow_s3GetObject_to_all.json
 }
+
 
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.static_website.id
@@ -58,6 +64,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
+# Make S3 bucket have public access since it will be a static website
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.static_website.id
 
@@ -67,6 +74,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
+# Create an S3 bucket ACL to have public access
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [
     aws_s3_bucket_ownership_controls.example,
@@ -77,6 +85,7 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "public-read"
 }
 
+# Upload the object (or the our htl file) from current working directory
 resource "aws_s3_object" "index_html" {
   bucket       = aws_s3_bucket.static_website.id
   key          = "index.html"
@@ -84,6 +93,7 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 }
 
+# Use the value this outputs on CLI and paste it to the browser
 output "website_url" {
   value = aws_s3_bucket_website_configuration.static-website-config.website_endpoint
 }
